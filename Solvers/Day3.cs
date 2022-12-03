c66c57100bbbc09d3d9ddf6142c1f66018986393
Johan Thorild <johan.thorild@consid.se>
2022-12-03 23:57:07 +0100

    Day 3

--- Solvers/Day3.cs
+++ Solvers/Day3.cs
@@ -5,11 +5,62 @@
     {
         public void Solve(string input)
         {
-            int score = 0;
-            int score2 = 0;
+            int part1 = 0;
+            int part2 = 0;
 
-            Console.WriteLine($"Part 1: {score}");
-            Console.WriteLine($"Part 2: {score2}");
+            // Part 1
+
+            var rucksacks = input.Split(Environment.NewLine);
+
+            var priorityMap = new Dictionary<char, int>();
+            InitPriorityMapper(priorityMap);
+
+            for (int i = 0; i < rucksacks.Length; i++)
+            {
+                int length = rucksacks[i].Trim().Length;
+                int halfLength = length / 2;
+
+                var firstPart = rucksacks[i][..halfLength].ToCharArray();
+                var secondPart = rucksacks[i][halfLength..].ToCharArray();
+                var matchedItemType = firstPart.Intersect(secondPart).Single();
+
+                part1 += priorityMap[matchedItemType];
+            }
+
+            // Part 2
+
+            char[] a, b, c;
+            for (int i = 0; i < rucksacks.Length; i += 3)
+            {
+                a = rucksacks[i].ToCharArray();
+                b = rucksacks[i + 1].ToCharArray();
+                c = rucksacks[i + 2].ToCharArray();
+                var matchingGroupItemType = a.Intersect(b).Intersect(c).Single();
+
+                part2 += priorityMap[matchingGroupItemType];
+            }
+
+            Console.WriteLine($"Part 1: {part1}");
+            Console.WriteLine($"Part 2: {part2}");
+        }
+
+        static void InitPriorityMapper(Dictionary<char, int> map)
+        {
+            int value = 1;
+
+            char lower = 'a';
+            while (lower <= 'z')
+            {
+                map.Add(lower, value++);
+                ++lower;
+            }
+
+            char upper = 'A';
+            while (upper <= 'Z')
+            {
+                map.Add(upper, value++);
+                ++upper;
+            }
         }
     }
 }
