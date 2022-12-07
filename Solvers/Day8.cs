13b041326d694ed431b62c1ff87427754a8ef836
Johan Thorild <johan.thorild@consid.se>
2022-12-07 23:31:28 +0100

    Prep day 8

---
+++ Solvers/Day8.cs
@@ -0,0 +1,22 @@
+﻿namespace Solvers;
+
+// https://adventofcode.com/2022/day/8
+public class Day8 : ISolver
+{
+    public int Part1 { get; set; } = 0;
+    public int Part2 { get; set; } = 0;
+
+    public void Solve(string input)
+    {
+        // Init
+
+        // Part 1
+
+        Console.WriteLine($"Part 1: {Part1}");
+
+
+        // Part 2
+
+        Console.WriteLine($"Part 2: {Part2}");
+    }
+}
